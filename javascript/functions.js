a50d1636bc40f2649c5a0c261f7204877d421cf7
mg-blvd2 <mguijarro@csumb.edu>
2020-03-01 10:41:19 -0800

    Added extra logic for when an API call returns less than 4 hits

--- javascript/functions.js
+++ javascript/functions.js
@@ -2,23 +2,31 @@ var imageIdx = []
 var possibleWords = ["dogs", "cats", "mice", "birds", "horses", "boats", "sky", "food"]
 let randIdx = Math.floor(Math.random() * 100 % 8);
 
-function getRandoms(){
+function getRandoms(imageCount){
   imageIdx = [];
   var holder;
+  if(imageCount < 4){
+    return;
+  }
   for(var i = 0; i < 4; i++){
-    holder = Math.floor((Math.random() * 100) % 20);
+    holder = Math.floor((Math.random() * 100) % imageCount);
     while(true){
       if(!imageIdx.includes(holder)){
         imageIdx.push(holder);
         break;
       }
+      if(holder === imageCount - 1){
+        holder = 0;
+      }
+      else{
       holder++;
+      }
     }
   }
 }
 
 function addImages(info){
-  getRandoms();
+  getRandoms(info.hits.length);
   $("#warningMes").hide();
   try{
     $(".imgLocations").html(`<div class="singleImg"><p>Likes: ${info.hits[imageIdx[0]].likes}</p><img src="${info.hits[imageIdx[0]].webformatURL}" alt="Image 1"/></div>`)
